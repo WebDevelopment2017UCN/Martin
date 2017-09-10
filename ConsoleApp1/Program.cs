39ad48752079bb1d5b962afdce7ad8d580b9c3c4
Martin Hlavacka <martinhlavacka@outlook.com>
2017-09-10 11:31:16 +0200

    Good day

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(knock knock) ... Who´s there?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "! Hope you have a great day ;) (yes/no)");
+            string leave = Console.ReadLine();
+            if (leave == "yes")
+            {
+                Console.WriteLine("Great! Enjoy it :))");
+            }
+            else
+            {
+                Console.WriteLine("Get some coffee, it will get better :)");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
